f605d13755f01ef23581fd213e51eba052bc8e29
aivan2798 <cot2we@gmail.com>
2023-11-30 12:57:44 +0300

    Update main.py
    time to fly

--- src/main.py
+++ src/main.py
@@ -25,20 +25,19 @@ def main(context):
         # Send a response with the res object helpers
         # `ctx.res.send()` dispatches a string back to the client
         return context.res.send("Hello, World!")
-
-    if context.req.method == "POST":
-        post_body = conext.req.body
-        url_link = post_body["content"]
-        url_sum_data = worker.eatUrl(url_link)
-        return context.res.json(url_sum_data)
+    #if context.req.method == "POST":
+    post_body = conext.req.body
+    url_link = post_body["content"]
+    url_sum_data = worker.eatUrl(url_link)
+    return context.res.json(url_sum_data)
     
 
     # `ctx.res.json()` is a handy helper for sending JSON
-    return context.res.json(
-        {
-            "motto": "Build like a team of hundreds_",
-            "learn": "https://appwrite.io/docs",
-            "connect": "https://appwrite.io/discord",
-            "getInspired": "https://builtwith.appwrite.io",
-        }
-    )
+    #return context.res.json(
+    #    {
+    #        "motto": "Build like a team of hundreds_",
+    #        "learn": "https://appwrite.io/docs",
+    #        "connect": "https://appwrite.io/discord",
+    #        "getInspired": "https://builtwith.appwrite.io",
+    #    }
+    #)
